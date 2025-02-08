57e5594acb9d518ba8ea3be5608a6ed2fa358eac
d4vvv <kacper.godula99@gmail.com>
2025-02-08 23:42:31 +0100

    test

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,8 +1,16 @@
 import { storyblokEditable } from '@storyblok/react'
+import { StoryblokServerComponent } from '@storyblok/react/rsc'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const LandingPage = (props: any) => {
-  console.log({ props })
+  // console.log({ props })
 
-  return <div {...storyblokEditable(props.blok)}>{props.blok.header}</div>
+  return (
+    <main {...storyblokEditable(props.blok)}>
+      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
+      {props.blok.blocks.map((blok: any) => (
+        <StoryblokServerComponent blok={blok} key={blok._uid} />
+      ))}
+    </main>
+  )
 }
